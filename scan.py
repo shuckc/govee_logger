877e7144837c249301966d4ae4e00cef7795d773
Chris Shucksmith <chris@shucksmith.co.uk>
2022-01-16 22:21:34 +0000

    fix connection leak, improve logging

--- scan.py
+++ scan.py
@@ -7,8 +7,7 @@ import struct
 
 
 def stripnull(data):
-    data = data.rstrip(b'\0')
-    return data.decode('UTF-8')
+    return data.rstrip(b'\0').decode('UTF-8')
 
 class DeviceFilter():
     @staticmethod
@@ -30,7 +29,7 @@ class DeviceFilter():
         return
 
     def __repr__(self):
-        return f"{self.__class__}, {self.device}"
+        return f"?? {self.device}"
 
 class Govee_H5174(DeviceFilter):
     # device has bluetooth
@@ -39,6 +38,9 @@ class Govee_H5174(DeviceFilter):
     def accept(device, advertisement_data) -> bool:
         name = advertisement_data.local_name
         return name is not None and name.startswith("GVH5174_")
+
+    def __repr__(self):
+        return f"Govee H5174 {self.device.address}"
 
     # AdvertisementData(local_name='GVH5174_6BE0',
     # manufacturer_data={1: b'\x01\x01\x02\xf7\xd6d', 76: b'\x02\x15INTELLI_ROCKS_HWPu\xf2\xff\xc2'},
@@ -49,9 +51,10 @@ class Govee_H5174(DeviceFilter):
         ds, = struct.unpack(">i", b'\x00' + dx[2:5])
         temp = (ds//1000)/10
         humid = (ds%1000)/10
-        print(f"Govee H5174 mac={self.device.address} temp={temp} humid={humid} bat={dx[5]}%")
+        print(f"{self} temp={temp} humid={humid} bat={dx[5]}%")
         self.ads.add(dx)
         return {'temp': temp, 'humid': humid, 'bat': dx[5]}
+
 
 class Govee_H5179(DeviceFilter):
     # device has Wifi, bluetooth,
@@ -61,6 +64,9 @@ class Govee_H5179(DeviceFilter):
         name = advertisement_data.local_name
         return name is not None and name.startswith("Govee_H5179_")
 
+    def __repr__(self):
+        return f"Govee H5179 {self.device.address}"
+
     # AdvertisementData(local_name='Govee_H5179_E0E2',
     # manufacturer_data={34817: b'\xec\x00\x01\x01\xea\x06\xd6\x15X'},
     # service_uuids=['0000180a-0000-1000-8000-00805f9b34fb', '0000fef5-0000-1000-8000-00805f9b34fb', '0000ec88-0000-1000-8000-00805f9b34fb'])
@@ -69,7 +75,7 @@ class Govee_H5179(DeviceFilter):
         if dx:
             assert dx[0:4].hex() == 'ec000101'
             temp, humid, bat = struct.unpack("<hhb", dx[4:])
-            print(f"Govee H5179 mac={self.device.address} temp={temp/100} humid={humid/100} bat={bat}%")
+            print(f"{self} temp={temp/100} humid={humid/100} bat={bat}%")
             self.ads.add(dx)
             return {'temp': temp/100, 'humid': humid/100, 'bat': bat}
         return {}
@@ -85,6 +91,7 @@ class Govee_H5179(DeviceFilter):
         await client.write_gatt_char(umisc, bytes.fromhex("AA0D0000000000000000000000000000000000A7"))
         await asyncio.sleep(0.5)
         await client.stop_notify(umisc)
+        await client.disconnect()
         return meta
 
     def handler_2011(self, meta, handle, data):
@@ -100,45 +107,45 @@ class Govee_H5179(DeviceFilter):
 
 
     async def do_download(self):
-        print('starting download')
         client = BleakClient(self.device.address, timeout=30)
         res = await client.connect()
+        print(f'{self} connected for download')
         ureq = '494e5445-4c4c-495f-524f-434b535f2012'
         ubulk = '494e5445-4c4c-495f-524f-434b535f2013'
         event = asyncio.Event()
-        print('starting download 2')
 
         await client.start_notify(ubulk, functools.partial(self.handler_2013, None))
         await client.start_notify(ureq, functools.partial(self.handler_2012, event))
         tfrom = 27365606
         tto   = 27366342
         await client.write_gatt_char(ureq, struct.pack('<hII', 0, tfrom, tto))
-        print(f'waiting for bulk download from {tfrom} to {tto}')
+        print(f'{self} waiting for bulk data from {tfrom} to {tto}')
         await event.wait()
 
         await client.stop_notify(ureq)
         await client.stop_notify(ubulk)
+        await client.disconnect()
 
     def handler_2012(self, finished, handle, data):
         # print(f"VR < handle={handle} data={data}")
         v, = struct.unpack("<b", data)
         if v == 2:
-            print('download finished')
+            print('Download finished')
             finished.set()
         elif v == 0:
-            print('download accepted')
+            print('Download accepted')
         elif v == 1:
             print('Download request failed! (lower bound too low?)')
             finished.set()
         else:
-            print(f'unknown download status: {v}')
+            print(f'unknown download status: {v}!')
 
     def handler_2013(self, results, handle, data):
         # print(f"VR < handle={handle} data={data}")
         index, t1, h1, t2, h2, t3, h3, t4, h4 = struct.unpack("<ihhhhhhhh", data)
         ts = datetime.fromtimestamp(index*60)
         if t1 == -1:
-            print(f' {index} {ts} --          t1={t2/100} t2={t3/100} t3={t4/100}')
+            print(f' {index} {ts} --      t1={t2/100} t2={t3/100} t3={t4/100}')
         else:
             print(f' {index} {ts} t1={t1/100} t2={t2/100} t3={t3/100} t4={t4/100}')
 
@@ -191,12 +198,14 @@ async def probe_devs(queue):
             d = await queue.get()
             if d is None:
                 return
-            print(f"interogating {d}")
-
+            print(f"Interogating {d}")
             # print(d.ads)
-            print(await d.get_meta())
-            queue.task_done()
+            md = await d.get_meta()
+            print(f"{d} metadata: {md}")
+            print(f"Starting download from {d}")
             print(await d.do_download())
+
+            queue.task_done()
         except Exception:
             logging.exception("ohno")
 
